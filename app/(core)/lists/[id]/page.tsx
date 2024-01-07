1481a1349ccfda19556200675cd721c32687c27e
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-01-06 22:29:23 -0800

    sdasdhjakldjasd

--- app/(core)/lists/[id]/page.tsx
+++ app/(core)/lists/[id]/page.tsx
@@ -2,6 +2,7 @@ import { cookies } from 'next/headers'
 import { notFound, redirect } from 'next/navigation'
 
 import Code from '@/components/Code'
+import Avatar from '@/components/core/Avatar'
 import ListItemRow from '@/components/lists/ListItemRow'
 
 import { isDeployed } from '@/utils/environment'
@@ -12,14 +13,10 @@ export default async function List({ params }: { params: { id: string } }) {
 	const supabase = createClient(cookieStore)
 	let { data, error } = await supabase
 		.from('lists')
-		.select('name,type,listItems(*)')
+		.select('name,type,listItems(*),users(email,raw_user_meta_data->name)')
 		.eq('id', params.id)
 		.not('active', 'is', false)
 		.single()
-	// .from('listItems')
-	// .select('*,lists(name, type)')
-	// .eq('list_id', params.id)
-	// .not('lists.active', 'is', false)
 
 	if (error) {
 		console.error(error)
@@ -27,19 +24,24 @@ export default async function List({ params }: { params: { id: string } }) {
 	}
 
 	const items = data?.listItems
+	const user = data?.users
 
 	return (
 		<div className="w-full animate-in flex-1 flex flex-col opacity-0 max-w-4xl px-3">
 			<div className="flex-1 flex flex-col gap-6">
-				<h1>{data?.name}</h1>
+				<div className="flex flex-row gap-2 items-center">
+					{/* @ts-expect-error */}
+					<Avatar name={user?.name || user?.email} />
+					<h1>{data?.name}</h1>
+				</div>
 
 				<div className="container mx-auto px-4">
 					<div className="flex flex-col">
-						{items?.length === 0 && <p className="text-gray-500 dark:text-gray-400">No lists yet.</p>}
+						{items?.length === 0 && <p className="text-gray-500 dark:text-gray-400">Nothing to see here... yet</p>}
 						<div className="flex flex-col">{items?.map(item => <ListItemRow key={item.id} item={item} />)}</div>
 					</div>
 				</div>
-				{!isDeployed && <Code code={JSON.stringify(items, null, 2)} />}
+				{!isDeployed && <Code code={JSON.stringify(data, null, 2)} />}
 			</div>
 		</div>
 	)
