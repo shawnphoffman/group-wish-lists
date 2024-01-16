d5b5a00066c5ec6a5045e5ae259cc218493950c3
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-01-15 17:45:11 -0800

    Badge in header

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 
 import { getUser, signOut } from '@/app/actions/auth'
 
+import Badge from './Badge'
+
 export default async function AuthButton() {
 	const { data: currentUser } = await getUser()
 
@@ -15,7 +17,9 @@ export default async function AuthButton() {
 
 	return (
 		<div className="flex items-center gap-4">
-			<span className="hidden sm:block">Hey, {currentUser.display_name || currentUser.email}!</span>
+			<Badge colorLabel={currentUser.display_name} className="!text-sm">
+				{currentUser.display_name}
+			</Badge>
 			<form action={signOut}>
 				<button className="nav-btn red">Logout</button>
 			</form>
