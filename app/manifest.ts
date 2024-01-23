cf74bde77415fd6c712d891114abc1d1f5891adb
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-01-23 09:16:21 -0800

    Update

--- app/manifest.ts
+++ app/manifest.ts
@@ -24,7 +24,7 @@ export default function manifest(): MetadataRoute.Manifest {
 		],
 		scope: '/',
 		start_url: '/',
-		display: 'minimal-ui',
+		display: 'standalone',
 		shortcuts: [
 			{
 				name: 'My Stuff',
