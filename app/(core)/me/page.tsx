6c7aff8b1aa9ebc5d8d97c213bddf08defd975fe
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-01-24 08:52:43 -0800

    Modal

--- app/(core)/me/page.tsx
+++ app/(core)/me/page.tsx
@@ -1,4 +1,4 @@
-import dynamic from 'next/dynamic'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { Suspense } from 'react'
 
@@ -7,13 +7,15 @@ import { getUser } from '@/app/actions/auth'
 import Badge from '@/components/common/Badge'
 import FallbackRow from '@/components/common/Fallbacks'
 import FontAwesomeIcon from '@/components/icons/FontAwesomeIcon'
+import CreateListModalNew from '@/components/me/CreateListModalNew'
 import MyLists from '@/components/me/MyLists'
 import MyPurchases from '@/components/me/MyPurchases'
 import PasswordForm from '@/components/me/PasswordForm'
 import ProfileForm from '@/components/me/ProfileForm'
-import PrelineScript from '@/components/utils/PrelineScript'
 
-const CreateListModal = dynamic(() => import('@/components/me/CreateListModal'), { ssr: false })
+type Props = {
+	searchParams: Record<string, string> | null | undefined
+}
 
 const MyStuffClient = async () => {
 	const userPromise = getUser()
@@ -40,27 +42,32 @@ const MyStuffClient = async () => {
 				<ProfileForm name={user.display_name} id={user.user_id} />
 			</div>
 			<div className="flex flex-col gap-3">
-				{/* <h3>Change Password</h3> */}
 				<PasswordForm id={user.user_id} />
 			</div>
 		</div>
 	)
 }
 
-export default async function MyStuff() {
+export default async function MyStuff({ searchParams }: Props) {
+	const show = searchParams?.show
 	return (
 		<>
-			<div className="flex flex-col flex-1 w-full max-w-xl px-3 opacity-0 animate-in">
+			<div className="flex flex-col flex-1 w-full max-w-2xl px-3 opacity-0 animate-in">
 				<main className="flex flex-col flex-1 gap-8 divide-y">
 					{/* LISTS */}
 					<div className="flex flex-col gap-3">
 						{/* Header */}
 						<div className="flex flex-row justify-between">
 							<h1>My Lists</h1>
-							<button type="button" className="mt-0 nav-btn blue" data-hs-overlay="#hs-create-list-modal">
+
+							<Link href="/me?show=true" className="mt-0 nav-btn blue">
 								<FontAwesomeIcon className="fa-sharp fa-solid fa-plus" />
 								Create List
-							</button>
+							</Link>
+							{/* <button type="button" className="mt-0 nav-btn blue" data-hs-overlay="#hs-create-list-modal">
+								<FontAwesomeIcon className="fa-sharp fa-solid fa-plus" />
+								Create List
+							</button> */}
 						</div>
 						{/* Lists */}
 						<Suspense fallback={<FallbackRow />}>
@@ -85,8 +92,7 @@ export default async function MyStuff() {
 					</div>
 				</main>
 			</div>
-			<PrelineScript />
-			<CreateListModal />
+			{show && <CreateListModalNew />}
 		</>
 	)
 }
---
+++ components/me/CreateListModalNew.tsx
@@ -0,0 +1,132 @@
+'use client'
+
+import { RadioGroup } from '@headlessui/react'
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+import { startTransition, useEffect, useRef } from 'react'
+// @ts-expect-error
+import { useFormState, useFormStatus } from 'react-dom'
+
+import { createList } from '@/app/actions/lists'
+
+import FontAwesomeIcon from '@/components/icons/FontAwesomeIcon'
+import ListTypeIcon from '@/components/icons/ListTypeIcon'
+
+import { ListCategory, ListPrivacy } from '@/utils/enums'
+
+// TODO Refactor Styles
+
+function CreateListFields() {
+	const { pending } = useFormStatus()
+
+	return (
+		<fieldset disabled={pending} className="flex flex-col bg-gray-800 border border-gray-700 shadow-2xl pointer-events-auto rounded-xl ">
+			<div className="flex items-center justify-between px-4 py-3 border-b dark:border-gray-700">
+				<h3 className="text-xl font-bold text-gray-800 dark:text-white">Create a List</h3>
+				<Link
+					href="/me"
+					className="flex items-center justify-center text-sm font-semibold text-gray-800 border border-transparent rounded-full w-7 h-7 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+				>
+					<span className="sr-only">Close</span>
+					<FontAwesomeIcon className="fa-sharp fa-solid fa-xmark" />
+				</Link>
+			</div>
+			<div className="flex flex-col gap-4 p-4 overflow-y-auto">
+				<div>
+					<label htmlFor="input-name" className="block mb-2 label">
+						List Title
+					</label>
+					<input type="text" id="input-name" name="list-name" placeholder="Your Cool List" autoFocus />
+				</div>
+				<div>
+					<RadioGroup name="list-privacy" defaultValue={ListPrivacy.Public}>
+						<RadioGroup.Label className="block mb-1 label">Privacy</RadioGroup.Label>
+						<div className="flex flex-col gap-1 sm:gap-2 sm:flex-row">
+							{/* TODO refactor */}
+							<RadioGroup.Option value={ListPrivacy.Public} key={'Public'}>
+								{({ checked }) => (
+									<label
+										className={`btn text-base mt-0 mb-1 flex-1 w-full ${checked ? ' teal ring-1 ring-white' : 'transparent'}`}
+										htmlFor={'Public'}
+									>
+										Public
+									</label>
+								)}
+							</RadioGroup.Option>
+							<RadioGroup.Option value={ListPrivacy.Private} key={'Private'}>
+								{({ checked }) => (
+									<label
+										className={`btn text-base mt-0 mb-1 flex-1 w-full ${checked ? ' teal ring-1 ring-white' : 'transparent'}`}
+										htmlFor={'Private'}
+									>
+										Private
+									</label>
+								)}
+							</RadioGroup.Option>
+						</div>
+					</RadioGroup>
+				</div>
+				<div>
+					<RadioGroup name="list-type" defaultValue={ListCategory.Christmas}>
+						<RadioGroup.Label className="block mb-1 label">List Type</RadioGroup.Label>
+						<div className="flex flex-col flex-wrap gap-1 sm:gap-2 sm:flex-row">
+							{Object.entries(ListCategory).map(
+								([key, value], i) =>
+									value !== 'test' && (
+										<RadioGroup.Option value={value} key={key}>
+											{({ checked }) => (
+												<label
+													className={`btn text-base mt-0 mb-1 flex-1 w-full ${checked ? ' teal ring-1 ring-white' : 'transparent'}`}
+													htmlFor={value}
+												>
+													<ListTypeIcon type={value} />
+													{key}
+												</label>
+											)}
+										</RadioGroup.Option>
+									)
+							)}
+						</div>
+					</RadioGroup>
+				</div>
+			</div>
+			<div className="flex items-center justify-end px-4 py-3 border-t gap-x-2 dark:border-gray-700">
+				<button type="button" className="btn gray">
+					Cancel
+				</button>
+				<button className="btn green">{pending ? 'Creating...' : 'Create List'}</button>
+			</div>
+		</fieldset>
+	)
+}
+
+export default function CreateListModal() {
+	const [state, formAction] = useFormState(createList, {})
+	const router = useRouter()
+	const pathname = usePathname()
+	const formRef = useRef<HTMLFormElement>(null)
+	console.log('CreateListModalNew', { state, pathname })
+
+	useEffect(() => {
+		if (state?.status === 'success') {
+			startTransition(() => {
+				if (formRef?.current) {
+					formRef.current.reset()
+				}
+				router.push('/me')
+			})
+		}
+	}, [state, pathname, router])
+
+	return (
+		<div className="fixed inset-0 flex items-center justify-center w-full h-full overflow-y-auto bg-gray-900 bg-opacity-70">
+			<form
+				action={formAction}
+				ref={formRef}
+				className="z-[80] overflow-x-hidden overflow-y-auto pointer-events-none transition-all ease-out"
+			>
+				<CreateListFields />
+			</form>
+		</div>
+	)
+}
