bb446dff6457cdba2cf1d4272571a383ee7d6cd4
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-02-02 08:07:38 -0800

    Fix image picker borders on dark

--- components/items/components/ItemImage.tsx
+++ components/items/components/ItemImage.tsx
@@ -6,5 +6,5 @@ type Props = {
 export default function ItemImage({ url, className = '' }: Props) {
 	if (!url) return null
 	// eslint-disable-next-line @next/next/no-img-element
-	return <img src={url} alt="" className={`object-scale-down rounded-lg border dark:border-none bg-white ${className}`} />
+	return <img src={url} alt="" className={`object-scale-down rounded-lg border bg-white ${className}`} />
 }
