fc767fa4108b51ffd16e9689a370a5893e081c80
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-02-17 22:09:53 -0800

    Add birthdays to grouped lists

--- components/lists/ListsByUser.tsx
+++ components/lists/ListsByUser.tsx
@@ -7,6 +7,10 @@ import ListBlock from '@/components/lists/ListBlock'
 import Badge from '../common/Badge'
 import { List } from '../types'
 
+const birthday = (month: string, day: number) => {
+	return `${month.charAt(0).toUpperCase() + month.slice(1)} ${day}`
+}
+
 export default async function ListsByUser() {
 	// const fakePromise = new Promise(resolve => setTimeout(resolve, 5000))
 	const userPromise = getSessionUser()
@@ -26,9 +30,12 @@ export default async function ListsByUser() {
 				{groupedLists?.map(group => (
 					<div key={`group-${group.id}`} className={`flex flex-col gap-1`}>
 						{/* <h2 className="mb-2 text-2xl dark:text-white">{group.display_name}</h2> */}
-						<Badge className="self-start !text-base" colorId={group.id}>
-							{group.display_name}
-						</Badge>
+						<div className="flex flex-row items-center gap-1">
+							<Badge className="!text-base" colorId={group.id}>
+								{group.display_name}
+							</Badge>
+							<Badge className="!text-xs gray">{birthday(group.birth_month, group.birth_day)}</Badge>
+						</div>
 						<ListBlock lists={group.lists as List[]} isOwner={currentUser?.id === group.user_id} />
 					</div>
 				))}
