a48e5fcb62d6db8568729bd2666b47b63af33cef
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-03-26 16:17:17 -0700

    Admin

---
+++ src/app/(core)/admin/layout.tsx
@@ -0,0 +1,18 @@
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+type Props = { children: React.ReactNode }
+
+export default async function AdminLayout({ children }: Props) {
+	const cookieStore = cookies()
+	const supabase = createClient(cookieStore)
+	const { data } = await supabase.from('view_me').select('is_admin').single()
+
+	if (!data?.is_admin) {
+		return redirect('/')
+	}
+
+	return <>{children}</>
+}
