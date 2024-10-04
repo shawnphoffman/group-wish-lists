06adeddec53a61030c6dc1f574447f66ab248384
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-10-04 10:47:16 -0700

    t5

--- src/components/items/components/ItemImagePicker.tsx
+++ src/components/items/components/ItemImagePicker.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import './ItemImagePicker.css'
-
-import { faXmark } from '@awesome.me/kit-ac8ad9255a/icons/sharp/solid'
+import { faImageSlash } from '@awesome.me/kit-ac8ad9255a/icons/sharp/solid'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { RadioGroup } from '@headlessui/react'
 
@@ -26,7 +24,7 @@ export default function ItemImagePicker({ images, imageUrl, setImageUrl }: Props
 						{({ checked }) => (
 							<ItemImage
 								url={image}
-								className={` max-h-48 border-4 no-drag ${checked ? 'filter-none bg-white border-yellow-500 ' : 'grayscale unselected'}`}
+								className={`max-h-48 border-4 ${checked ? 'filter-none bg-white border-yellow-500 ' : 'grayscale text-gray-500 border-gray-300 dark:border-gray-700'}`}
 							/>
 						)}
 					</RadioGroup.Option>
@@ -34,12 +32,12 @@ export default function ItemImagePicker({ images, imageUrl, setImageUrl }: Props
 				<RadioGroup.Option value={''}>
 					{({ checked }) => (
 						<div
-							className={`w-48 aspect-square max-h-48 flex items-center justify-center border-4 rounded-lg ${
+							className={`w-32 aspect-square max-h-32 flex items-center justify-center border-4 rounded-lg ${
 								checked ? 'border-red-500 text-white' : 'unselected'
 							}`}
 						>
 							<div className="flex flex-col items-center text-4xl">
-								<FontAwesomeIcon icon={faXmark} />
+								<FontAwesomeIcon icon={faImageSlash} className="text-red-400" />
 								<span className="text-xl">No Image</span>
 							</div>
 						</div>
