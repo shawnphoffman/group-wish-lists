6120fcce23a349cadccfdfdd0ec4fd6f7bf51ede
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-10-04 18:31:48 -0700

    manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		short_name: 'Wish Lists',
+		name: 'Group Wish Lists',
+		description: 'Sharing wish lists made easy.',
+		scope: '/',
+		start_url: '/',
+		display: 'standalone',
+		shortcuts: [
+			{
+				name: 'My Stuff',
+				short_name: 'My Stuff',
+				description: 'My lists, purchases, and profile info',
+				url: '/me?source=pwa',
+			},
+		],
+	}
+}
