4b5111942dd4376b41fef680d3a5b9237e0bf77e
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-10-14 09:23:25 -0700

    colors

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -23,31 +23,68 @@ type IconProps = {
 } & Partial<FontAwesomeIconProps>
 
 export function OpenUrlIcon() {
-	return <FontAwesomeIcon icon={faUpRightFromSquare} className="text-teal-400 hover:text-teal-300" />
+	return (
+		<FontAwesomeIcon icon={faUpRightFromSquare} className="text-teal-500 hover:text-teal-600 dark:text-teal-400 dark:hover:text-teal-300" />
+	)
 }
 export function EditIcon() {
-	return <FontAwesomeIcon icon={faPenToSquare} className="text-yellow-400 hover:text-yellow-300 group-hover:text-yellow-300" />
+	return (
+		<FontAwesomeIcon
+			icon={faPenToSquare}
+			className="text-yellow-500 hover:text-yellow-600 group-hover:text-yellow-600 dark:text-yellow-400 dark:hover:text-yellow-300 dark:group-hover:text-yellow-300"
+		/>
+	)
 }
 export function DeleteIcon() {
-	return <FontAwesomeIcon icon={faTrashXmark} fixedWidth className="text-red-400 hover:text-red-300 group-hover:text-red-300" />
+	return (
+		<FontAwesomeIcon
+			icon={faTrashXmark}
+			fixedWidth
+			className="text-red-500 hover:text-red-600 group-hover:text-red-600 dark:text-red-400 dark:hover:text-red-300 dark:group-hover:text-red-300"
+		/>
+	)
 }
 export function BroomIcon() {
-	return <FontAwesomeIcon icon={faBroom} fixedWidth className="text-fuchsia-400 hover:text-fuchsia-300 group-hover:text-fuchsia-300" />
+	return (
+		<FontAwesomeIcon
+			icon={faBroom}
+			fixedWidth
+			className="text-fuchsia-500 hover:text-fuchsia-600 group-hover:text-fuchsia-600 dark:text-fuchsia-400 dark:hover:text-fuchsia-300 dark:group-hover:text-fuchsia-300"
+		/>
+	)
 }
 export function UnarchiveIcon() {
-	return <FontAwesomeIcon icon={faEye} fixedWidth className="text-teal-400 hover:text-teal-300" />
+	return (
+		<FontAwesomeIcon icon={faEye} fixedWidth className="text-teal-500 hover:text-teal-600 dark:text-teal-600 dark:hover:text-teal-300" />
+	)
 }
 export function ArchiveIcon() {
-	return <FontAwesomeIcon icon={faEyeSlash} fixedWidth className="text-lime-400 hover:text-lime-300" />
+	return (
+		<FontAwesomeIcon
+			icon={faEyeSlash}
+			fixedWidth
+			className="text-lime-500 hover:text-lime-600 dark:text-lime-400 dark:hover:text-lime-300"
+		/>
+	)
 }
 export function ImportIcon() {
-	return <FontAwesomeIcon icon={faFileImport} className="text-violet-400 hover:text-violet-300" />
+	return (
+		<FontAwesomeIcon
+			icon={faFileImport}
+			className="text-violet-500 hover:text-violet-600 dark:text-violet-400 dark:hover:text-violet-300"
+		/>
+	)
 }
 export function AddIcon() {
-	return <FontAwesomeIcon icon={faCirclePlus} className="text-blue-400 hover:text-blue-300" />
+	return <FontAwesomeIcon icon={faCirclePlus} className="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-300" />
 }
 export function CancelIcon() {
-	return <FontAwesomeIcon icon={faBan} className="text-orange-400 hover:text-orange-300 group-hover:text-orange-300" />
+	return (
+		<FontAwesomeIcon
+			icon={faBan}
+			className="text-orange-500 hover:text-orange-600 group-hover:text-orange-300 dark:text-orange-400 dark:hover:text-orange-300 dark:group-hover:text-orange-300"
+		/>
+	)
 }
 export function LockIcon({ className, ...rest }: IconProps) {
 	return (
@@ -56,7 +93,7 @@ export function LockIcon({ className, ...rest }: IconProps) {
 			fade
 			style={{ '--fa-animation-duration': '2s', '--fa-fade-opacity': '0.75' } as CSSProperties}
 			{...rest}
-			className={`text-violet-300 ${className}`}
+			className={`text-violet-600 dark:text-violet-300 ${className}`}
 		/>
 	)
 }
@@ -67,19 +104,23 @@ export function PermissionsIcon({ className, ...rest }: IconProps) {
 			icon={faUserUnlock}
 			fixedWidth
 			{...rest}
-			className={`text-amber-300 hover:text-amber-400 group-hover:text-amber-400 ${className}`}
+			className={`text-amber-600 hover:text-amber-500 group-hover:text-amber-500 dark:text-amber-300 dark:hover:text-amber-400 dark:group-hover:text-amber-400 ${className}`}
 		/>
 	)
 }
 export function ShareIcon({ className, ...rest }: IconProps) {
-	return <FontAwesomeIcon icon={faUserGroup} {...rest} className={`text-emerald-300 ${className}`} />
+	return <FontAwesomeIcon icon={faUserGroup} {...rest} className={`text-emerald-600 dark:text-emerald-300 ${className}`} />
 }
 export function MoveIcon({ className, ...rest }: IconProps) {
-	return <FontAwesomeIcon icon={faRightLongToLine} {...rest} className={`text-purple-300 ${className}`} />
+	return <FontAwesomeIcon icon={faRightLongToLine} {...rest} className={`text-purple-600 dark:text-purple-300 ${className}`} />
 }
 export function FlushIcon({ className, ...rest }: IconProps) {
 	return (
-		<FontAwesomeIcon icon={faBoxArchive} {...rest} className={`text-sky-300 hover:text-sky-400 group-hover:text-sky-400 ${className}`} />
+		<FontAwesomeIcon
+			icon={faBoxArchive}
+			{...rest}
+			className={`text-sky-600 hover:text-sky-500 group-hover:text-sky-500 dark:text-sky-300 dark:hover:text-sky-400 dark:group-hover:text-sky-400 ${className}`}
+		/>
 	)
 }
 export function LoadingIcon({ className, ...rest }: IconProps) {
--- src/components/icons/ListTypeIcon.tsx
+++ src/components/icons/ListTypeIcon.tsx
@@ -15,21 +15,21 @@ type Props = {
 }
 
 export default function ListTypeIcon({ type, className = 'text-xl' }: Props) {
-	let iconColor = 'text-yellow-300'
+	let iconColor = 'text-yellow-600 dark:text-yellow-300'
 	let icon = faThoughtBubble
 
 	if (type === ListCategory.Birthday) {
 		icon = faBirthdayCake
-		iconColor = 'text-pink-300'
+		iconColor = 'text-pink-600 dark:text-pink-300'
 	} else if (type === ListCategory.Christmas) {
 		icon = faTreeChristmas
-		iconColor = 'text-green-300'
+		iconColor = 'text-green-600 dark:text-green-300'
 	} else if (type === ListCategory.Test) {
 		icon = faVialVirus
-		iconColor = 'text-blue-300'
+		iconColor = 'text-blue-600 dark:text-blue-300'
 	} else if (type === ListCategory.GiftIdeas) {
 		icon = faLightbulb
-		iconColor = 'text-teal-300'
+		iconColor = 'text-teal-600 dark:text-teal-300'
 	}
 
 	return <FontAwesomeIcon icon={icon} className={`${iconColor} ${className}`} />
