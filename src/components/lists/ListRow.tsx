36b9a914174f39a9da0d07e67ab7e8e4385369ea
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-10-16 18:51:33 -0700

    Styling fixes

--- src/components/lists/ListRow.tsx
+++ src/components/lists/ListRow.tsx
@@ -1,14 +1,12 @@
 import { faList, faStar } from '@awesome.me/kit-ac8ad9255a/icons/sharp/solid'
-import { faLockKeyhole } from '@awesome.me/kit-ac8ad9255a/icons/sharp-duotone/solid'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 
 import ListTypeIcon from '@/components/icons/ListTypeIcon'
-// import ArchiveListButton from '@/components/lists/buttons/ArchiveListButton'
-// import DeleteListButton from '@/components/lists/buttons/DeleteListButton'
 import { List, ListSharedWithMe, ListSharedWithOthers } from '@/components/types'
 import { Badge } from '@/components/ui/badge'
 
+// import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 import { LockIcon, ShareIcon } from '../icons/Icons'
 
 type Props = {
@@ -19,7 +17,7 @@ type Props = {
 const CountBadge = ({ count }: { count: number }) => (
 	<Badge
 		variant={'outline'}
-		className={`hidden gap-x-1.5 py-1 px-3 text-xs whitespace-nowrap xs:inline-flex ${count > 0 ? '' : 'text-muted-foreground'}`}
+		className={`gap-x-1.5 py-1 px-3 text-xs whitespace-nowrap inline-flex ${count > 0 ? '' : 'text-muted-foreground'}`}
 	>
 		<FontAwesomeIcon icon={faList} className="!hidden sm:!inline" />
 		{count}
@@ -41,38 +39,47 @@ export default function ListRow({ list, canEdit }: Props) {
 	// console.log('ListRow', list)
 
 	return (
-		<div className={`xs:!text-lg flex-row p-2 bg-transparent hover:bg-muted rounded flex ${privateClasses}`}>
-			<LinkOrDiv href={url!} className={`flex flex-row flex-1 items-center gap-2`}>
-				<ListTypeIcon type={list.type} />
-				<div className={`${!isActive ? 'line-through opacity-50' : ''} leading-none`}>{list.name}</div>
-				{list?.private && <LockIcon className="text-sm" />}
-				{isShared && <ShareIcon className="text-sm" />}
-				{isPrimary && <FontAwesomeIcon icon={faStar} className="text-yellow-500" />}
+		<div className={`!text-lg flex-row p-0 bg-transparent hover:bg-muted rounded flex ${privateClasses}`}>
+			<LinkOrDiv href={url!} className={`flex flex-row flex-1 items-center gap-2 overflow-hidden`}>
+				<div className="flex flex-row items-center flex-1 gap-2 overflow-hidden">
+					<ListTypeIcon type={list.type} />
+					<div className={`${!isActive ? 'line-through opacity-50' : ''} leading-none text-ellipsis whitespace-nowrap overflow-hidden`}>
+						{list.name}
+					</div>
+					{/* <TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger
+							className={`${!isActive ? 'line-through opacity-50' : ''} leading-none text-ellipsis whitespace-nowrap overflow-hidden`}
+						>
+							{list.name}
+						</TooltipTrigger>
+						<TooltipContent className="max-w-60 text-pretty">{list.name}</TooltipContent>
+					</Tooltip>
+				</TooltipProvider> */}
+					{list?.private && <LockIcon className="text-sm" />}
+					{isShared && <ShareIcon className="text-sm" />}
+					{isPrimary && <FontAwesomeIcon icon={faStar} className="text-yellow-500" />}
+				</div>
+
+				<div className="flex flex-row items-center justify-end gap-1 !text-lg">
+					{/*  */}
+					{(list as ListSharedWithOthers).user_shared_with_id && (
+						<Badge variant="outline" className="!text-[10px] bg-muted whitespace-nowrap">
+							{/*  */}
+							w/{(list as ListSharedWithOthers).user_shared_with_display_name}
+						</Badge>
+					)}
+					{/*  */}
+					{(list as ListSharedWithMe).sharer_display_name && (
+						<Badge variant="outline" className="!text-[10px] bg-muted whitespace-nowrap">
+							{/*  */}
+							For: {(list as ListSharedWithMe).sharer_display_name}
+						</Badge>
+					)}
+					{/*  */}
+					<CountBadge count={list.count!} />
+				</div>
 			</LinkOrDiv>
-			<div className="flex flex-row items-center justify-end gap-1 !text-lg">
-				{/*  */}
-				{(list as ListSharedWithOthers).user_shared_with_id && (
-					<Badge variant="outline" className="!text-[10px] bg-muted whitespace-nowrap">
-						{/*  */}
-						w/{(list as ListSharedWithOthers).user_shared_with_display_name}
-					</Badge>
-				)}
-				{/*  */}
-				{(list as ListSharedWithMe).sharer_display_name && (
-					<Badge variant="outline" className="!text-[10px] bg-muted whitespace-nowrap">
-						{/*  */}
-						For: {(list as ListSharedWithMe).sharer_display_name}
-					</Badge>
-				)}
-				{/* {list?.private && <FontAwesomeIcon icon={faLockKeyhole} fixedWidth className="text-sm text-emerald-300" />} */}
-				<CountBadge count={list.count!} />
-				{/* {canEdit && (
-					<>
-						<ArchiveListButton listId={list.id} isArchived={!isActive} />
-						<DeleteListButton listId={list.id} name={list.name} />
-					</>
-				)} */}
-			</div>
 		</div>
 	)
 }
