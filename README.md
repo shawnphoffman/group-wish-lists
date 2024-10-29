bece73a1ba8b9c68dad74f1ce26cbe709bf06a55
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-10-29 10:09:46 -0400

    Remove sentry

--- README.md
+++ README.md
@@ -4,14 +4,4 @@ This is a small passion project to help organize wish lists for my family and fr
 
 ## Ideas
 
-- Edit: Add amazon wish list importer
-
-  - https://www.amazon.com/hz/wishlist/ls/2QDX7UX6DZ86O?ref_=wl_share
-  - https://www.amazon.com/hz/wishlist/dl/invite/2pKVDQ8?ref_=wl_share
-  - https://api.shawn.party/api/amazon/wish-list/scrape?url=https://www.amazon.com/hz/wishlist/ls/3TBI1L7T5L35V/ref=nav_wishlist_lists_1
-
-- Event end dates to clear out purchased items from lists
 - List: Add items that you purchased to someone else's list
-- Core: Guest users
-- Core: Permissions for who can view what users
-- My Stuff: View everything you've received
--- next.config.js
+++ next.config.js
@@ -1,52 +1,9 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true',
-})
-
-// const { withSentryConfig } = require('@sentry/nextjs')
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+// 	enabled: process.env.ANALYZE === 'true',
+// })
 
 /** @type {import('next').NextConfig} */
-let nextConfig = {
-	// eslint: {
-	// dirs: ['pages', 'utils', 'app'],
-	// },
-}
+let nextConfig = {}
 
-// nextConfig = withSentryConfig(
-// 	module.exports,
-// 	{
-// 		// For all available options, see:
-// 		// https://github.com/getsentry/sentry-webpack-plugin#options
-
-// 		// Suppresses source map uploading logs during build
-// 		silent: true,
-// 		org: 'shawn-hoffman',
-// 		project: 'wish-lists',
-// 	},
-// 	{
-// 		// For all available options, see:
-// 		// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-// 		// Upload a larger set of source maps for prettier stack traces (increases build time)
-// 		widenClientFileUpload: true,
-
-// 		// Transpiles SDK to be compatible with IE11 (increases bundle size)
-// 		transpileClientSDK: true,
-
-// 		// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-// 		// tunnelRoute: '/monitoring',
-
-// 		// Hides source maps from generated client bundles
-// 		hideSourceMaps: true,
-
-// 		// Automatically tree-shake Sentry logger statements to reduce bundle size
-// 		disableLogger: true,
-
-// 		// Enables automatic instrumentation of Vercel Cron Monitors.
-// 		// See the following for more information:
-// 		// https://docs.sentry.io/product/crons/
-// 		// https://vercel.com/docs/cron-jobs
-// 		automaticVercelMonitors: true,
-// 	}
-// )
-
-module.exports = withBundleAnalyzer(nextConfig)
+// module.exports = withBundleAnalyzer(nextConfig)
+module.exports = nextConfig
