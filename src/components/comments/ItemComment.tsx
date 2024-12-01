2c93577f4cd8e07566954b9bc3d188a188a80de8
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-12-01 10:40:01 -0800

    Add links to comments

--- src/components/comments/ItemComment.tsx
+++ src/components/comments/ItemComment.tsx
@@ -4,6 +4,7 @@ import { useCallback, useState } from 'react'
 import { faComment } from '@awesome.me/kit-ac8ad9255a/icons/sharp/solid'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { formatDistance } from 'date-fns'
+import Linkify from 'linkify-react'
 import { useRouter } from 'next/navigation'
 
 import { deleteComment } from '@/app/actions/comments'
@@ -15,6 +16,11 @@ import { Button } from '@/components/ui/button'
 
 type Props = {
 	comment: Comment
+}
+
+const linkOptions = {
+	className: 'underline hover:text-primary',
+	target: '_blank',
 }
 
 export default function ItemComment({ comment }: Props) {
@@ -43,7 +49,10 @@ export default function ItemComment({ comment }: Props) {
 				</Badge>
 			</AlertTitle>
 			<AlertDescription className="flex flex-row items-center justify-between">
-				<div>{comments}</div>
+				{/* <div className="[&_a]:underline [&_a]:hover:text-primary"> */}
+				<div>
+					<Linkify options={linkOptions}>{comments}</Linkify>
+				</div>
 				{isOwner && (
 					<Button variant="outline" size="sm" className="w-8 h-8 p-2" onClick={handleDelete} disabled={loading}>
 						{loading ? <LoadingIcon size="lg" /> : <DeleteIcon />}
