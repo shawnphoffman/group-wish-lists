18e675a16957ced12344163559f135322f858d5d
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2025-11-11 00:29:07 -0800

    Recent items row improvements

--- src/components/items/ItemRowSimple.tsx
+++ src/components/items/ItemRowSimple.tsx
@@ -1,31 +1,26 @@
-import { faLink } from '@awesome.me/kit-f973af7de0/icons/sharp/solid'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 
 import { getSessionUser } from '@/app/actions/auth'
-import { getUserById } from '@/app/actions/users'
 import ItemPriorityIcon from '@/components/icons/PriorityIcon'
-import ItemCheckbox from '@/components/items/components/ItemCheckbox'
 import ItemImage from '@/components/items/components/ItemImage'
-import { Gift, ListItem } from '@/components/types'
 import { Badge } from '@/components/ui/badge'
 import { formatDateBasedOnAge } from '@/utils/date'
 import { ItemPriority, ItemStatus } from '@/utils/enums'
-import { getDomainFromUrl } from '@/utils/urls'
 
 import AddCommentButton from '../comments/AddCommentButton'
 import ItemComments from '../comments/ItemComments'
 
-import ItemGifters from './components/ItemGifters'
 import MarkdownBlock from './components/MarkdownBlock'
 import ItemRowActions from './ItemRowActions'
+import { formatDistance } from 'date-fns/formatDistance'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
 type Props = {
-	item: ListItem & Gift
-	isOwnerView: boolean
+	// item: ListItem & Gift
+	item: any
 }
 
-export default async function ItemRow({ item, isOwnerView }: Props) {
+export default async function ItemRowSimple({ item }: Props) {
 	if (!item) return null
 
 	if (item.archived) return null
@@ -39,28 +34,16 @@ export default async function ItemRow({ item, isOwnerView }: Props) {
 		// fakePromise
 	])
 
-	// console.log('ListItemRow', { item, users })
-
-	const isComplete = !isOwnerView && item.status === ItemStatus.Complete
-	// const userCanChange = item?.gifter_user_id === currentUser?.id || item.status !== ItemStatus.Complete
-
-	// const completeClass = isComplete ? (userCanChange ? 'opacity-75' : 'opacity-50') : ''
-	const completeClass = ''
-
-	const additionalGifters = await Promise.all(item?.additional_gifter_ids?.map(async g => await getUserById(g)) || [])
+	// console.log('ListItemSimple', { item })
 
 	return (
-		<div className={`flex flex-col w-full gap-2 p-3 hover:bg-muted ${completeClass}`} id={`item-${item.id}`}>
+		<div className={`flex flex-col w-full gap-2 p-3 hover:bg-muted`} id={`item-${item.id}`}>
 			<div className="flex flex-col w-full gap-2">
 				<div className="flex flex-row items-stretch gap-x-3.5">
-					{/* Priority & Checkbox */}
+					{/* Priority */}
 					<div className="flex flex-col items-center justify-center gap-2 shrink-0">
 						{/* Priority */}
 						{item.priority !== ItemPriority.Normal && <ItemPriorityIcon priority={item.priority} />}
-						{/* Checkbox */}
-						{!isOwnerView && (
-							<ItemCheckbox id={item.id} status={item.status} requestedQty={item.quantity || 1} currentUserId={currentUser!.id} />
-						)}
 					</div>
 					{/*  */}
 					<div className="flex max-[400px]:flex-col min-[401px]:items-center flex-1 gap-2 xs:flex-row md:gap-4">
@@ -71,18 +54,18 @@ export default async function ItemRow({ item, isOwnerView }: Props) {
 								{item.url ? (
 									<Link
 										href={item.url!}
-										target="_blank"
+										// target="_blank"
 										className={`flex flex-col sm:flex-row sm:items-center gap-0.5 sm:gap-1 overflow-hidden hover:underline `}
 									>
 										{item.title}
-										<div className="flex flex-row items-center gap-1">
+										{/* <div className="flex flex-row items-center gap-1">
 											<FontAwesomeIcon
 												icon={faLink}
 												className="text-teal-500 hover:text-teal-600 dark:text-teal-400 dark:hover:text-teal-300"
 												size="xs"
 											/>
 											<span className="flex text-xs text-muted-foreground">{getDomainFromUrl(item.url)}</span>
-										</div>
+										</div> */}
 									</Link>
 								) : (
 									<div>{item.title}</div>
@@ -109,26 +92,25 @@ export default async function ItemRow({ item, isOwnerView }: Props) {
 									<MarkdownBlock>{item.notes}</MarkdownBlock>
 								</div>
 							)}
-							{item.created_at && (
-								<div className="text-xs italic break-words whitespace-pre-line text-muted-foreground/50">
-									Added: {formatDateBasedOnAge(item.created_at)}
-								</div>
-							)}
-
-							{/* Gifter */}
-							{!isOwnerView && (
-								<ItemGifters id={item.id} requestedQty={item.quantity || 1} />
-								// <div className="flex flex-row items-center gap-1 mt-1 text-sm">
-								// 	<Badge variant={'outline'} className="flex flex-row items-center leading-tight">
-								// 		{item.display_name}
-								// 	</Badge>
-								// 	{additionalGifters.map(g => (
-								// 		<Badge key={g.user_id} variant={'outline'} className="flex flex-row items-center leading-tight">
-								// 			+{g.display_name}
-								// 		</Badge>
-								// 	))}
-								// </div>
-							)}
+							<div className="flex flex-col gap-2 xs:items-center xs:flex-row">
+								<Avatar className="w-5 h-5 border border-foreground">
+									<AvatarImage src={item?.user?.image} />
+									<AvatarFallback className="text-xl font-bold bg-background text-foreground">
+										{item.user.display_name?.charAt(0)}
+									</AvatarFallback>
+								</Avatar>
+								<Badge variant="outline" className="text-muted-foreground whitespace-nowrap w-fit">
+									{item.user.display_name}
+								</Badge>
+								{item.created_at && (
+									<div
+										className="text-xs italic break-words whitespace-pre-line text-muted-foreground/50"
+										title={formatDateBasedOnAge(item.created_at)}
+									>
+										Added: {formatDistance(new Date(item.created_at), new Date(), { addSuffix: true })}
+									</div>
+								)}
+							</div>
 						</div>
 
 						{/* Image + Actions */}
